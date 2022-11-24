c452cc41c446c23f5784e54aca4ab626e41d74f9
Vitor Henrique <116174168+VitorMalacarne@users.noreply.github.com>
2022-11-24 14:43:25 +0000

    Alterações finais, projeto concluído.

--- .ccls-cache/@home@runner@entrega-7/header.h
+++ .ccls-cache/@home@runner@entrega-7/header.h
@@ -4,13 +4,14 @@
 
 struct Tcarro {
   char modelo[20], cor[20], cliente[30];
-  int ano;
+  int ano, comb_c;
 };
 
 void flush_in();
 void espera();
-void imprimir(struct Tcarro vetor[], int t);
+void imprimir(struct Tcarro vetor[], int t, int logico, float v_comb);
 void addcarro(int *fila, int t_fila, struct Tcarro Sfila[]);
-void abastecimento(int *tanque, int *fila, float v_comb, int *ca_atnd,
+int abastecimento(int *tanque, int *fila, float v_comb, int *ca_atnd,
                    struct Tcarro Sfila[], struct Tcarro Sca_atnd[]);
-void relatorio(int tanque, int ca_atnd, float v_comb, struct Tcarro Sca_atnd[]);
+void relatorio(int tanque, int ca_atnd, float v_comb, struct Tcarro Sca_atnd[],
+               int logico);
